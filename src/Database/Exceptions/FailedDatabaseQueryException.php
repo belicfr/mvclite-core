5b269f12da98f7ee87864fee2926fc7da1addf80
Jonathan <96206878+belicfr@users.noreply.github.com>
2024-02-28 13:47:22 +0100

    Added values display for failed db query exception

--- src/Database/Exceptions/FailedDatabaseQueryException.php
+++ src/Database/Exceptions/FailedDatabaseQueryException.php
@@ -16,12 +16,22 @@ use MvcliteCore\Engine\MvcLiteException;
  */
 class FailedDatabaseQueryException extends MvcLiteException
 {
-    public function __construct(string $query)
+    public function __construct(string $query, array $values)
     {
         parent::__construct();
 
+        $valuesAsString = [];
+
+        foreach ($values as $value)
+        {
+            $valuesAsString[] = var_export($value, true);
+        }
+
+        $values = implode(", ", $valuesAsString);
+
         $this->code = "MVCLITE_DB_FAILED_QUERY";
         $this->message = "<strong>Database Error:</strong> Failed to run database query.<br />
-                          <strong>Query:</strong> $query";
+                          <strong>Query:</strong> $query<br>
+                          <strong>With values:</strong> $values";
     }
 }
