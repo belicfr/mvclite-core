291c2fa3acd289cdcc5b4030a3da9a62a4a71d0d
Jonathan <96206878+belicfr@users.noreply.github.com>
2024-02-28 15:02:04 +0100

    Created API class
    To construct and send api requests.

---
+++ src/API/API.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace MvcliteCore\API;
+
+class API
+{
+    public const NOT_FOUND = 404;
+
+    public const SUCCESS = 200;
+
+    private array $data;
+
+    public function __construct()
+    {
+        $this->data = [];
+    }
+
+    public function getAllData(): array
+    {
+        return $this->data;
+    }
+
+    public function getData(string $key): mixed
+    {
+        return $this->getAllData()[$key] ?? null;
+    }
+
+    public function setData(string $key, mixed $value): void
+    {
+        $this->data[$key] = $value;
+    }
+
+    public function removeData(string $key): void
+    {
+        if (in_array($key, $this->getAllData()))
+        {
+            $this->data = array_slice($this->data, 0, -1);
+        }
+    }
+
+    /**
+     * Prepare the API response.
+     *
+     * @param int $status Request HTTP status
+     * @return APIResponse
+     */
+    public function response(int $status): APIResponse
+    {
+        $response = new APIResponse($status, $this->getAllData());
+        $response->send();
+
+        return $response;
+    }
+
+    /**
+     * Prepare a not found API response.
+     *
+     * @return APIResponse
+     * @see API::response()
+     * @see API::NOT_FOUND
+     */
+    public function respondNotFound(): APIResponse
+    {
+        return $this->response(self::NOT_FOUND, $this->getAllData());
+    }
+
+    /**
+     * Prepare a success API response.
+     *
+     * @return APIResponse
+     * @see API::response()
+     * @see API::SUCCESS
+     */
+    public function respondSuccess(): APIResponse
+    {
+        return $this->response(self::SUCCESS, $this->getAllData());
+    }
+
+    public static function open(string $api): API
+    {
+        return new $api();
+    }
+}
