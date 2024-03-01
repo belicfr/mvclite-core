6aedb074404b7e6bad29c46355ac1b2d7176a96e
Jonathan <96206878+belicfr@users.noreply.github.com>
2024-03-01 09:37:00 +0100

    Create VarcharColumn.php

---
+++ src/Database/ORM/Migrator/Builder/String/VarcharColumn.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace MvcliteCore\Database\ORM\Migrator\Builder\String;
+
+class VarcharColumn extends StringColumn
+{
+    private const DEFINITION_TEMPLATE = "`%s` VARCHAR(%s) %s";
+
+    private const DEFAULT_MAX_LENGTH = 1;
+
+    private int $maxLength;
+
+    public function __construct(string $name)
+    {
+        parent::__construct($name);
+
+        $this->maxLength = self::DEFAULT_MAX_LENGTH;
+    }
+
+    /**
+     * @return string Column SQL creation script
+     */
+    public function getCreationSql(): string
+    {
+        return sprintf(self::DEFINITION_TEMPLATE,
+                       $this->getName(),
+                       $this->getMaxLength(),
+                       $this->getConstraintsAsString());
+    }
+
+    public function getMaxLength(): int
+    {
+        return $this->maxLength;
+    }
+
+    public function maxLength(int $length): VarcharColumn
+    {
+        if ($length > 0)
+        {
+            $this->maxLength = $length;
+        }
+
+        return $this;
+    }
+
+    /**
+     * Creates new column for migration.
+     *
+     * @param string $name Column name
+     * @return VarcharColumn New column
+     */
+    public static function make(string $name): VarcharColumn
+    {
+        return new VarcharColumn($name);
+    }
+}
