c04e1cce195c1fcd2858e41a8b6574da362d82b0
Jonathan <96206878+belicfr@users.noreply.github.com>
2024-03-05 23:19:44 +0100

    Removed plugin name argument from exception
    already using

--- src/Plugins/Exceptions/PluginAlreadyExistsException.php
+++ src/Plugins/Exceptions/PluginAlreadyExistsException.php
@@ -6,12 +6,11 @@ use MvcliteCore\Engine\MvcLiteException;
 
 class PluginAlreadyExistsException extends MvcLiteException
 {
-    public function __construct(string $pluginName, string $currentPluginClassName = "Unknown")
+    public function __construct(string $plugin)
     {
         parent::__construct();
 
         $this->code = "MVCLITE_ALREADY_EXISTING_PLUGIN";
-        $this->message = "$pluginName plugin is already existing.<br />
-                          <strong>Current plugin class:</strong> $currentPluginClassName";
+        $this->message = "$plugin plugin is already existing.";
     }
 }
